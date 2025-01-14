c62aac66b1b91b7dadc50483f5c8626498b9852b
Damisola Morawo <morawodamisolataiwo77@gmail.com>
2025-01-14 12:09:01 +0100

    feat:user input textfield added

--- lib/features/navigation/router.dart
+++ lib/features/navigation/router.dart
@@ -5,13 +5,14 @@ import 'package:glovo_assign1/features/mini_project/pages/result_page.dart';
 import 'package:glovo_assign1/features/navigation/route_strings.dart';
 import 'package:glovo_assign1/features/pages/sign_up_screen.dart';
 import 'package:glovo_assign1/features/tabbar_view/tab_bar_screen.dart';
+import 'package:glovo_assign1/features/user_input/user_input_screen.dart';
 
 import '../pageview/page_view.dart';
 
 Route<dynamic> appRouter(RouteSettings settings) {
   switch (settings.name) {
     case AppRouteStrings.base:
-      return CupertinoPageRoute(builder: (_) => const LightModePage());
+      return CupertinoPageRoute(builder: (_) => const UserInputScreen());
     case AppRouteStrings.carouselScreen:
       return CupertinoPageRoute(builder: (_) => const CarouselScreen());
     case AppRouteStrings.resultScreen:
@@ -22,6 +23,8 @@ Route<dynamic> appRouter(RouteSettings settings) {
       return CupertinoPageRoute(builder: (_) => const PageViewScreen());
     case AppRouteStrings.tabBarViewScreen:
       return CupertinoPageRoute(builder: (_) => const TabBarScreen());
+    case AppRouteStrings.userInputScreen:
+      return CupertinoPageRoute(builder: (_) => const UserInputScreen());
 
     default:
       return CupertinoPageRoute(builder: (_) => const LightModePage());
--- lib/features/user_input/user_input_screen.dart
+++ lib/features/user_input/user_input_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:glovo_assign1/features/constants.dart';
 
 class UserInputScreen extends StatefulWidget {
   const UserInputScreen({super.key});
@@ -17,11 +19,14 @@ class _UserInputScreenState extends State<UserInputScreen> {
   final _ageController = TextEditingController();
 
   final _formKey = GlobalKey<FormState>();
+
+  bool viewPassword = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("User input"),
+        centerTitle: true,
       ),
       body: Form(
         key: _formKey,
@@ -42,6 +47,11 @@ class _UserInputScreenState extends State<UserInputScreen> {
                   TextFormField(
                     controller: _fullNameController,
                     focusNode: _fullNameNode,
+                    keyboardType: TextInputType.name,
+                    textCapitalization: TextCapitalization.words,
+                    inputFormatters: [
+                      FilteringTextInputFormatter.allow(RegExp(r'[a-z A-Z]'))
+                    ],
                     cursorColor: Colors.grey,
                     decoration: InputDecoration(
                         filled: true,
@@ -54,7 +64,89 @@ class _UserInputScreenState extends State<UserInputScreen> {
                         focusedBorder: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(12),
                             borderSide: const BorderSide(color: Colors.blue))),
-                  )
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    controller: _emailController,
+                    focusNode: _emailNode,
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (a) {
+                      if (emailRegex.hasMatch(a ?? "")) {
+                        return null;
+                      } else {
+                        return "Invalid email";
+                      }
+                    },
+                    inputFormatters: [
+                      FilteringTextInputFormatter.deny(RegExp(r'[]'))
+                    ],
+                    cursorColor: Colors.grey,
+                    decoration: InputDecoration(
+                      filled: true,
+                      hintText: "eg Damisola@gmail.com",
+                      label: const Text("Email Address"),
+                      fillColor: Colors.blue.withOpacity(.1),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: const BorderSide(color: Colors.blue)),
+                      errorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: const BorderSide(color: Colors.red)),
+                      focusedErrorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: const BorderSide(color: Colors.red)),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    controller: _passwordController,
+                    focusNode: _passwordNode,
+                    cursorColor: Colors.grey,
+                    obscuringCharacter: "☢",
+                    obscureText: viewPassword,
+                    keyboardType: TextInputType.visiblePassword,
+                    enableSuggestions: false,
+                    decoration: InputDecoration(
+                        suffixIcon: GestureDetector(
+                            onTap: () {
+                              viewPassword = !viewPassword;
+                              setState(() {});
+                            },
+                            child: Icon(viewPassword
+                                ? Icons.visibility_off_outlined
+                                : Icons.visibility_outlined)),
+                        filled: true,
+                        hintText: "****",
+                        label: const Text("Password"),
+                        fillColor: Colors.blue.withOpacity(.1),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: const BorderSide(color: Colors.blue))),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Center(
+                      child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 30,
+                    ),
+                    child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState?.validate() ?? false) {}
+                        },
+                        child: const Text("Save")),
+                  ))
                 ],
               ),
             ),
